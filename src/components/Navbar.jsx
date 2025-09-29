bb2348dffd2170ef3996a3086bf119e2c38c4583
ellamonacillo <ellamonacillo@gmail.com>
2025-09-29 11:01:40 +0800

    Add: Hero section 1st part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,19 +8,20 @@ const Navbar = () => {
     const navTween = gsap.timeline({
         scrollTrigger: {
             trigger: 'nav',
-            start: 'bottom nav'
+            start: 'bottom top'
         }
     });
 
     navTween.fromTo('nav', 
-        { backgroundColor: 'transparent' },
+        { backgroundColor: 'transparent' }, 
         { 
+            backgroundColor: '#00000050',
             backgroundFilter: 'blur(10px)',
             duration: 1,
             ease: 'power1.inOut'
         }
     )
-  });  
+  });
 
   return (
     <nav>
