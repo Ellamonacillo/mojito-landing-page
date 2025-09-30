fab65a2ef944d1eb8bcf24477e397bb01291accb
ellamonacillo <ellamonacillo@gmail.com>
2025-09-30 10:31:39 +0800

    Add: Hero section video animation

--- constants/index.js
+++ constants/index.js
@@ -81,11 +81,11 @@ const goodLists = [
 ];
 
 const storeInfo = {
-    heading: "Where to Find US",
+    heading: "Where to Find Us",
     address: "456, Raq Blvd. #404, Los Angeles, CA 90210",
     contact: {
         phone: "(555) 987-6543",
-        email: "hello@jsmcocktails.com"
+        email: "hello@mgmocktails.com"
     }
 };
 
